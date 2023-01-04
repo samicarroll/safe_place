2746b9c3160bdc390cf654435502be3d645efbf3
Gabriela Alvarez <69876801+gabrielaAlv1@users.noreply.github.com>
2023-01-03 19:22:21 -0500

    Update safe_place.py

--- safe_place.py
+++ safe_place.py
@@ -116,7 +116,8 @@ def init() -> str:
 
 
 headers = {
-    'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36'
+    'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36',
+    'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:108.0) Gecko/20100101 Firefox/108.0'
 }
 
 
