9aa5f3b6fd53bef909ec51343e2f3135acfb4db1
samicarroll <62121500+samicarroll@users.noreply.github.com>
2023-02-21 16:22:03 -0500

    Update megapersonals.py

--- megapersonals.py
+++ megapersonals.py
@@ -18,6 +18,7 @@ url = 'https://megapersonals.eu/'
 # options = webdriver.ChromeOptions()
 # options.add_argument("--headless=new")
 # driver = webdriver.Chrome(options=options)
+
 driver = webdriver.Chrome()  # TODO: USED IN TESTING - DELETE BEFORE DEPLOYMENT
 driver.get(url)
 timestamps = datetime.datetime.now().strftime('%m_%d_%y %H%M%S')
@@ -62,60 +63,67 @@ driver.implicitly_wait(5)
 fort_myers = driver.find_element(By.XPATH, '//*[@id="choseCityContainer"]/div[3]/article/div[10]/article/p[3]/a')
 driver.execute_script("arguments[0].click();", fort_myers)
 driver.implicitly_wait(5)
+
 # CLICK WOMEN SEARCHING MALE
 women_seeking_male = driver.find_element(By.XPATH, '//*[@id="megapCategoriesOrangeButton"]/a')
 driver.execute_script("arguments[0].click();", women_seeking_male)
 time.sleep(5)
 
 # LOOP THROUGH LISTINGS AND PULL DESIRED INFORMATION
-# container = driver.find_elements(By.ID, 'list')
-# for posts in container:
-
-# HOLDS LINKS
 listings = driver.find_elements(By.CLASS_NAME, 'listadd')
-# ENTERS EACH LISTING TO PULL INFORMATION
-# for post in listings:
+# HOLDS LINKS
 links = []
+# PULLS URLS FROM EACH LISTING
 for listing in listings:
     # BYPASS STALE ELEMENT
     try:
         urls = listing.find_element(By.TAG_NAME, 'a').get_attribute('href')
     except StaleElementReferenceException:
         pass
-    # counter = 0
-    # FIXME: ONLY GRABBING THE FIRST LINK
+    # TODO: ADD FUNCTION TO CLICK NEXT PAGE BUTTON
     links.append(urls)
-
 counter = 0
+
 for urls in links:
     driver.get(links[counter])
     counter += 1
+    screenshot = False
+
     # check if keyword is in the page source and takes screenshot with the keyword found
     for keyword in keywords:
         if keyword in driver.page_source:
-            print(links)  # TODO: TEST PRINT REMOVE BEFORE DEPLOYMENT
+            # INCLUDES POST TIME
+            title = driver.find_element(By.CLASS_NAME, 'post_preview_title').text
+            time.sleep(5)
+            print(title)
+
+            age = driver.find_element(By.CLASS_NAME, 'post_preview_age').text
+            time.sleep(5)
+            print(age)
+
+            description = driver.find_element(By.CLASS_NAME, 'postbody').text
+            time.sleep(5)
+            print(description)
+
+            phone_number = driver.find_element(By.CSS_SELECTOR, 'body > div > div.post_preview_body > div.fromLeft.post_preview_phone > span > a').get_attribute("innerHTML")
+            time.sleep(5)
+            print(phone_number)
+
+            # APPEND CONTENTS TO LIST
+            LIST.append([title, age, description, phone_number])
+
+            # SCREENSHOT LISTING
             screenshot_name = f"megapersonals_{counter}_keyword_{keyword.replace(' ', '_')}.png"
             driver.save_screenshot(screenshot_name)
+            screenshot = True
 
         # SET SCREENSHOT SIZE
-    S = lambda X: driver.execute_script('return document.body.parentNode.scroll' + X)
-    driver.set_window_size(S('Width'), S('Height'))
+        S = lambda X: driver.execute_script('return document.body.parentNode.scroll' + X)
+        driver.set_window_size(S('Width'), S('Height'))
 
-    # post_time = listing.find_element(By.CLASS_NAME, 'post_preview_date_time').text
-    # time.sleep(5)
-    # title = wait.until(EC.visibility_of_element_located(By.CLASS_NAME('post_preview_title'))).text
-    # time.sleep(5)
-    # age = listing.find_element(By.CSS_SELECTOR, 'body > div > div.isee-age > div.post_preview_age').text
-    # time.sleep(5)
-    # description = listing.find_element(By.CSS_SELECTOR, 'body > div > div.post_preview_body > span').text
-    # time.sleep(5)
-    # phone_number = listing.find_element(By.CSS_SELECTOR, 'body > div > div.post_preview_body > div.fromLeft.post_preview_phone > span > a').text
-    # time.sleep(5)
-    # APPEND CONTENTS TO LIST
-    # LIST.append([post_time, title, age, description])
 
 # EXPORT TO EXCEL FILE
-columns = ('time', 'title', 'age', 'description')
+columns = ('title', 'age', 'description', 'phone number')
 df = pd.DataFrame(LIST, columns=columns)
 
 df.to_excel(f'megapersonals({timestamps}).xlsx', index=False)
