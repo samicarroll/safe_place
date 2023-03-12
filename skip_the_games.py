3e93c8122983b698c0d8ec6f5b0df3441252f1a2
Gabriela Alvarez <69876801+gabrielaAlv1@users.noreply.github.com>
2023-03-11 19:34:22 -0500

    Update skip_the_games.py

--- skip_the_games.py
+++ skip_the_games.py
@@ -1,7 +1,9 @@
 import datetime
 import time
 
+import undetected_chromedriver
 from selenium import webdriver
+from selenium.webdriver.common import keys
 from selenium.webdriver.common.by import By
 import pandas as pd
 from selenium.webdriver.common.keys import Keys
@@ -11,73 +13,58 @@ from selenium.webdriver.support.ui import WebDriverWait
 
 timestamps = datetime.datetime.now().strftime('%m_%d_%y %H%M%S')
 url = 'https://skipthegames.com/'
-driver = webdriver.Chrome()
+driver = undetected_chromedriver.Chrome()
 driver.get(url)
 LIST = []
 
 # WAIT FOR ELEMENTS TO BE CLICKABLE
-wait = WebDriverWait(driver, 10)
+wait = WebDriverWait(driver, 30)
 
 # ACCEPT COOKIES
-cookie = driver.find_element(By.CLASS_NAME, 'cc-cookie-accept')
-driver.execute_script("arguments[0].click();", cookie)
-driver.implicitly_wait(10)
+cookie = wait.until(EC.element_to_be_clickable((By.CLASS_NAME, 'cc-cookie-accept')))
+cookie.click()
 
 # SELECT PREFERENCES
+# WAIT FOR PAGE TO FULLY LOAD
+wait.until(EC.element_to_be_clickable((By.NAME, 'input_search_client')))
 # SELF-IDENTIFICATION (MALE, WOMAN, ETC.)
 identification = Select(driver.find_element(By.NAME, 'input_search_client'))
 identification.select_by_value('men')
-driver.implicitly_wait(10)
 
 # LOOKING FOR: ESCORT,
-category = Select(driver.find_element(By.NAME, 'input_search_category'))
+category = Select(wait.until(EC.element_to_be_clickable((By.NAME, 'input_search_category'))))
 category.select_by_value('female-escorts')
-driver.implicitly_wait(10)
 
-# ENTERING FORT MYERS IN CITY
-location = driver.find_element(By.NAME, 'input_search_location')
-location.send_keys('fort myers')
-driver.implicitly_wait(10)
-
-# TODO: KEYWORDS CANNOT BE ENTERED BECAUSE
-#  PAGE REFRESHES AND REMOVES LOCATION
+# ENTERING FORT MYERS IN CITY TODO: page refresh before able to insert the location
+location = wait.until(EC.element_to_be_clickable((By.NAME, 'input_search_location')))
+driver.execute_script("arguments[0].value = 'Fort Myers';", location)
 
 # ENTER KEYWORDS
-# keywords = driver.find_element(By.NAME, 'input_search_optional_keywords')
-# keywords.send_keys('blonde')
+keywords = wait.until(EC.element_to_be_clickable((By.NAME, 'input_search_optional_keywords')))
+driver.execute_script("arguments[0].value = 'blonde';", keywords)
 
-# SEARCH LOCATION AND KEYWORDS
-location.send_keys(Keys.ENTER)  # search location
+# SEARCH
+wait.until(EC.element_to_be_clickable((By.ID, 'search_button'))).click()
+#location.send_keys(Keys.ENTER)  # search location
 
 # wait.until(EC.element_to_be_clickable((By.ID, 'search_button'))).click()
 
-gallery = driver.find_elements(By.CLASS_NAME, 'clsfds-display-mode gallery')
+gallery = wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, '.clsfds-display-mode.gallery')))
 for ads in gallery:
-    posts = driver.find_elements(By.CLASS_NAME, 'day-gallery')
+    posts = ads.find_elements(By.CSS_SELECTOR, '.day-gallery')
     for post in posts:
-        date = driver.find_element(By.CLASS_NAME, 'post-timestamp m10')
+        date = post.find_element(By.CLASS_NAME, 'post-timestamp').get_attribute('innerHTML')
         print(date)
 
-        title = driver.find_element(By.CLASS_NAME, 'post-title').text
+        title = post.find_element(By.CLASS_NAME, 'post-title').get_attribute('innerHTML')
         print(title)
 
-        description = driver.find_element(By.ID, 'post-body').text
+        description = post.find_element(By.ID, 'post-body').get_attribute('innerHTML')
         print(description)
 
-        age = driver.find_element(By.CLASS_NAME, 'tiny secondary radius label ad_display_h3_age')
+        age = post.find_element(By.CLASS_NAME, 'ad_display_h3_age').get_attribute('innerHTML')
         print(age)
 
         driver.get_screenshot_as_file(f"skipthegames({timestamps}).png")
 
-        # APPEND CONTENTS TO LIST
-        LIST.append([date, title, description, age])
-        time.sleep(1)
-
-# EXPORT TO EXCEL
-columns = ('date', 'title', 'description', 'age')
-df = pd.DataFrame(LIST, columns=columns)
-
-df.to_excel(f'skipthegames({timestamps}).xlsx', index=False)
-print(f'skipthegames({timestamps}).xlsx exported.')
-# CLOSE WEBDRIVER
 driver.close()
