f577004b33af82c5da14386ce00b8934784bd311
samicarroll <62121500+samicarroll@users.noreply.github.com>
2023-03-28 11:37:32 -0400

    Update skip_the_games.py
    updated file paths
    added phone number to excel file
    added counter to excel file

--- skip_the_games.py
+++ skip_the_games.py
@@ -1,9 +1,12 @@
 import datetime
 import time
 import pandas as pd
-import undetected_chromedriver
+import undetected_chromedriver as uc
 import ssl
-import openpyxl
+import re
+import pathlib
+
+from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.select import Select
 from selenium.webdriver.support.ui import WebDriverWait
@@ -23,20 +26,22 @@ headers = {
 }
 
 # SET UP HEADLESS PAGE
-# options = webdriver.ChromeOptions()
-# options.add_argument("--headless=new")
-# driver = undetected_chromedriver.Chrome(options=options)
-driver = undetected_chromedriver.Chrome()
+options = webdriver.ChromeOptions()
+options.add_argument("--headless=new")
+driver = uc.Chrome(options=options)
+# driver = uc.Chrome()
 driver.get(url)
 time.sleep(5)
 
 # WAIT FOR ELEMENTS TO BE CLICKABLE
 wait = WebDriverWait(driver, 10)
+
 # LIST FOR KEYWORDS
-file = open('keywords.txt', 'r')
+file = open('static/keywords.txt', 'r')
 lines = file.read()
 keywords = lines.split('\n')
 driver.refresh()
+
 # SELECT PREFERENCES
 # SELF-IDENTIFICATION (MALE, WOMAN, ETC.)
 identification = Select(driver.find_element(By.NAME, 'input_search_client'))
@@ -50,41 +55,53 @@ driver.implicitly_wait(10)
 driver.refresh()
 
 posts = driver.find_elements(By.CSS_SELECTOR, 'html.no-js body div table.two-col-wrap tbody tr '
-                             'td#gallery_view.listings-with-sidebar.list-search-results.gallery div.full-width '
-                             'div.small-16.columns div.clsfds-display-mode.gallery div.day-gallery [href]')
+                                              'td#gallery_view.listings-with-sidebar.list-search-results.gallery div.full-width '
+                                              'div.small-16.columns div.clsfds-display-mode.gallery div.day-gallery ['
+                                              'href]')
 dupLinks = [post.get_attribute('href') for post in posts]
 links = [*set(dupLinks)]
-print(links)
 counter = 0
+
 for urls in links:
     links[:] = (urls for urls in links if not urls.startswith('https://skipthegames.com/reply/meetup/'))
     driver.get(links[counter])
-
     time.sleep(5)
+
     description = driver.find_element(By.CSS_SELECTOR, '#post-body > div').text
+    time.sleep(5)
     for keyword in keywords:
         if keyword in description:
             ad_url = driver.current_url
-            print(ad_url)
 
             title = driver.find_element(By.CLASS_NAME, 'post-title').text
-            print(title)
+
+            pattern = r'(\(\d{3}\)\s*[-\.\s]\s*\d{3}\s*[-\.\s]??\s*\d{4}|\d{3}[-\.\s]??\d{3}[-\.\s]??\d{4}|\(\d{' \
+                      r'3}\)\s*\d{3}[-\.\s]??\d{4}|\d{3}[-\.\s]??\d{4})'
+            des = driver.find_element(By.CSS_SELECTOR, '#post-body > div').text
+            phone_number = re.findall(pattern, des)
+
+            # REMOVES DUPLICATES
+            phone_number = [*set(phone_number)]
 
             # APPEND CONTENTS TO LIST
-            LIST.append([ad_url, title])
+            LIST.append([counter, ad_url, title, phone_number, keyword])
 
             # SCREENSHOT LISTING
-            screenshot_name = f"skipthegames{counter}_keyword_{keyword.replace(' ', '_')}.png"
-            driver.save_screenshot(screenshot_name)
+            screenshot_name = f"({counter})skipthegames_keyword_{keyword.replace(' ', '_')}_({timestamps}).png"
+            driver.save_screenshot(pathlib.Path.home() / f"Desktop/skipthegames/screenshots/{screenshot_name}")
             break
+
+        # SET SCREENSHOT SIZE
+        S = lambda X: driver.execute_script('return document.body.parentNode.scroll' + X)
+        driver.set_window_size(S('Width'), S('Height'))
 
     counter += 1
 # SET UP COLUMNS FOR EXCEL FILE
-columns = ('URL', 'Title')
+columns = ('Screenshot Number', 'URL', 'Title', 'Phone Number', 'Matching Keyword')
 df = pd.DataFrame(LIST, columns=columns)
 
 # EXPORT TO EXCEL FILE
-df.to_excel(f'skipthegames({timestamps}).xlsx', index=False)
+df.to_excel(pathlib.Path.home() / f"Desktop/skipthegames/excel_files/skipthegames({timestamps}).xlsx", index=False)
 print(f'skipthegames({timestamps}).xlsx exported.')
 
 # CLOSE WEBDRIVER
